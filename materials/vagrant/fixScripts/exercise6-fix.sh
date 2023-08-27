d52254195f58586d5ae76873037ef3c29e1f35a7
davidDuch <david.duch.uni@gmail.com>
2023-08-28 00:49:03 +0300

    Solutions

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+if [ $# -lt 2 ]; then
+   echo "Less than 2 arguments"
+   exit 1
+fi
+
+dest="${!#}"
+
+for ((i = 1; i < $#; i++)); do
+   file="${!i}"
+   scp "$file" "$dest"
+done
+
+total_size=0
+for ((i = 1; i < $#; i++)); do
+   file="${!i}"
+   file_size=$(wc -c < "$file")
+   total_size=$((total_size + file_size))
+done
+
+echo "$total_size"
